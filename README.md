3b49dc0f95d9bd8256b2eb25751043ddb0b274f5
Zackbot2 <zacharycummings0714@gmail.com>
2025-03-26 10:20:07 -0600

    right-click menu button
    add button to the message right-click menu on plugin start

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # TestPlugin
+A simple BetterDiscord plugin made for the sole purpose of learning how to even make one.
--- TestPlugin.plugin.js
+++ TestPlugin.plugin.js
@@ -1,17 +1,49 @@
 /**
  * @name TestPlugin
  * @author Zackbot
- * @description Describe the basic functions. Maybe a support server link.
- * @version 0.0.1
+ * @description Try random shit out
+ * @version 0.0.2
+ * @authorid 449774710469689355
+ * @source https://github.com/Zackbot2/TestPlugin
  */
 
-module.exports = class YourPlugin {
-    start() {
-      // Called when the plugin is activated (including after reloads)
-      BdApi.alert("Hello World", "Plugin is active");
-    } 
+function createButton() {
 
-    stop() {
-      // Called when the plugin is deactivated
-    }
+  
+
+  const myButton = document.createElement("button");
+  myButton.textContent = "CLICK FOR SEX";
+  myButton.className = "sex-button"
+  myButton.addEventListener("click", () => {
+    window.alert("SEX");
+  });
+  const serverList = document.querySelector(".scroller_c1e9c4");
+  serverList.append(myButton);
+
+  BdApi.DOM.onRemoved(myButton, () => {
+    serverList.append(myButton);
+  });
 }
+
+module.exports = class YourPlugin {
+  start() {
+    // Called when the plugin is activated (including after reloads)
+    BdApi.alert("Guh??", "Plugin is active");
+
+    BdApi.DOM.addStyle( "SexButton", `.sex-button {
+      padding: 10px;
+      border: 4px solid black;
+      background-color: gray;
+      border-radius: 10px;
+      font-size: 10px;
+  
+      width: 150px;
+    }`);
+
+    createButton();
+  }
+
+  stop() {
+    //BdApi.DOM.removeStyle("SexButton")
+  }
+};
